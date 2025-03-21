6cdfe7957eca1ba629b0f4510adadab8e5cf7056
“Aaban <aabanqureshi564@gmail.com>
2025-03-21 23:54:16 +0500

    Created Genric Repository and Added its Interface, also addded .gitignore file to ignore bin and obj folder of .Server folder (debug builds etc)

---
+++ AIDoctor.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDoctor.Domain.Interfaces
+{
+    public interface IGenricRepository<TClass, Tkey>
+        where TClass : class
+    {
+        Task<TClass> GetByIdAsync(Tkey key);
+        Task<IEnumerable<TClass>> GetAllAsync();
+        Task AddAsync(TClass entity);
+        Task UpdateAsync(TClass entity);
+        Task DeleteAsync(Tkey key);
+        Task SaveAsync();
+
+    }
+}
