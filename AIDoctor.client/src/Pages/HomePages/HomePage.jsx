702494a2f07677aa788035ed58e7b1da8e335495
AbdulSattarMajid <malikabdulsattarmajid@gmail.com>
2025-04-07 00:51:16 +0500

    Fixed the Logo importing error

--- AIDoctor.client/src/Pages/HomePages/HomePage.jsx
+++ AIDoctor.client/src/Pages/HomePages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar'
 import Footer from '../../Components/Footer'
-import HomeSection from '../../Components/HomePart1'
-import WhyChooseChatbot from '../../Components/HomePart2'
-import FeaturesSection from '../../Components/HomePart3'
-import ContactForm from '../../Components/HomePart4'
-import Testimonials from '../../Components/HomePart5'
+import HomeSection from '../../Components/homePart1'
+import WhyChooseChatbot from '../../Components/homePart2'
+import FeaturesSection from '../../Components/homePart3'
+import ContactForm from '../../Components/homePart4'
+import Testimonials from '../../Components/homePart5'
 
 export default function HomePage() {
   return (
